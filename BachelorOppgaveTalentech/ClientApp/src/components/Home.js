82b5eb2af8644677aaf5cc0f0d600443ada0882d
kritibaniya <kreeti.baniya@gmail.com>
2023-02-02 12:12:17 +0100

    Opprettet ny developmentbranch. Her skal alt det nye kode legges inn før koden blir publisert i master branch. Denne commit består av nye controller metode for å lese appinfo fra blobcontainer. I tillegg er home.js oppdatert med kode for å vise alle app navn.

--- BachelorOppgaveTalentech/ClientApp/src/components/Home.js
+++ BachelorOppgaveTalentech/ClientApp/src/components/Home.js
@@ -2,4 +2,47 @@ import React, { Component } from 'react';
 
 export class Home extends Component {
     static displayName = Home.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { appInformation: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.getApplicationInformation();
+    }
+    
+    static renderAppInformationTable(appInformation) {
+        return (
+            <div>
+                <h2>List of names:</h2>
+                <ul>
+                    {appInformation.map((file, index) => (
+                        <li key={index}>{file.Name}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Home.renderAppInformationTable(this.state.appInformation);
+
+        return (
+            <div>
+                <h1 id="tableLabel">App names</h1>
+                 
+        {contents}
+            </div>
+        );
+    }
+
+    async getApplicationInformation() {
+        const response = await fetch('marketplaceadmin');
+        const data = await response.json();
+        console.log(data);
+        this.setState({ appInformation: data, loading: false });
+    }
 }
--- BachelorOppgaveTalentech/Controllers/MarketplaceAdminController.cs
+++ BachelorOppgaveTalentech/Controllers/MarketplaceAdminController.cs
@@ -20,6 +20,32 @@ namespace BachelorOppgaveTalentech.Controllers
     [Route("[controller]")]
     public class MarketplaceAdminController : ControllerBase
     {
-       
+        private readonly ILogger<MarketplaceAdminController> _logger;
+
+        private string _connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+        private string _containerName = "appsinfo";
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+
+
+            List<dynamic> blobs2 = new List<dynamic>();
+            var blobs = containerClient.GetBlobs();
+            foreach (var blob in blobs)
+            {
+                BlobClient blobClient = containerClient.GetBlobClient(blob.Name);
+                var response = blobClient.Download();
+
+                using var reader = new StreamReader(response.Value.Content);
+                var data = reader.ReadToEnd();
+                blobs2.Add(JsonSerializer.Deserialize<dynamic>(data));
+            }
+            Console.WriteLine(blobs2.ToArray());
+            return Ok(blobs2.ToArray());
+        }
     }
 }
