13f620434c1311a65bea513d764fc0901b2a31d5
kreet <kreet@LAPTOP-FLCFKCFG>
2023-02-02 11:49:59 +0100

    Add project files.

---
+++ BachelorOppgaveTalentech/Controllers/MarketplaceAdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace BachelorOppgaveTalentech.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MarketplaceAdminController : ControllerBase
+    {
+       
+    }
+}
